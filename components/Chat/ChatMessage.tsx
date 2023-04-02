1fa22abd900416ce41ca70468e47fa96e1797100
Jiaping Zhang <jiaping.zhang@gemini.com>
2023-04-02 11:48:11 -0700

    update chat role icon and feedback icon

--- components/Chat/ChatMessage.tsx
+++ components/Chat/ChatMessage.tsx
@@ -1,5 +1,5 @@
 import { Message } from '@/types/chat';
-import { IconCheck, IconCopy, IconEdit } from '@tabler/icons-react';
+import { IconCheck, IconCopy, IconEdit, IconUser, IconRobot, IconThumbUp, IconThumbDown } from '@tabler/icons-react';
 import { useTranslation } from 'next-i18next';
 import { FC, memo, useEffect, useRef, useState } from 'react';
 import rehypeMathjax from 'rehype-mathjax';
@@ -81,7 +81,7 @@ export const ChatMessage: FC<Props> = memo(
       >
         <div className="relative m-auto flex gap-4 p-4 text-base md:max-w-2xl md:gap-6 md:py-6 lg:max-w-2xl lg:px-0 xl:max-w-3xl">
           <div className="min-w-[40px] text-right font-bold">
-            {message.role === 'assistant' ? t('AI') : t('You')}:
+            {message.role === 'assistant' ? <IconRobot size={30}/> : <IconUser size={30}/>}
           </div>
 
           <div className="prose mt-[-2px] w-full dark:prose-invert">
@@ -216,6 +216,14 @@ export const ChatMessage: FC<Props> = memo(
                 >
                   {message.content}
                 </MemoizedReactMarkdown>
+                <div className="text-gray-400 flex self-end lg:self-center justify-center mt-2 gap-2 md:gap-3 lg:gap-1 lg:absolute lg:top-5 lg:translate-x-full lg:right-0 lg:mt-0 lg:pl-2 visible">
+                   <button className='p-1 rounded-md hover:bg-gray-100 hover:text-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-200 disabled:dark:hover:text-gray-400'>
+                    <IconThumbUp/>
+                   </button>
+                   <button className='p-1 rounded-md hover:bg-gray-100 hover:text-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-200 disabled:dark:hover:text-gray-400'>
+                   <IconThumbDown/>
+                   </button>
+                </div>
               </>
             )}
           </div>
